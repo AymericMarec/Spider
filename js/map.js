8c33da8e761aabcd2c949909d3138ffe0530914a
maymeric <aymeric.marec@ynov.com>
2024-10-28 23:06:15 +0100

    simple spider project done

--- js/map.js
+++ js/map.js
@@ -1,56 +1,62 @@
-import { AddSpider } from "./spider.js"
+import { Spider } from "./spider.js"
 
-function getRandomInt(min,max) {
-    return Math.floor(Math.random() * max) +min;
-}
-
-let canvas = document.getElementById("canvas")  
+export let canvas = document.getElementById("canvas")
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 export let context = canvas.getContext("2d");
 export let AllPoint = []
+let targetX = 200
+let targetY = 200
 
-//Create glowing point for spiders
-function CreatePoint(x,y){
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * max) + min;
+}
+
+//Create glowing point for spider
+function CreatePoint(x, y) {
     const newpoint = document.createElement("div");
     newpoint.classList.add("point")
-    newpoint.style.left =  y + "px"
-    newpoint.style.top = x+ "px"
-    AllPoint.push({point: newpoint,IsAvailable:true})
-    // AllPoint.push(newpoint)
+    newpoint.style.left = y + "px"
+    newpoint.style.top = x + "px"
+    AllPoint.push({ point: newpoint, IsAvailable: true, x: x, y: y })
+        // AllPoint.push(newpoint)
     document.body.appendChild(newpoint)
 }
 
 //function wich return clost glowing point from coordinates
-export function GetClosestPoint(x,y){
+export function GetClosestPoint(y, x) {
     let BestDistance = 100000
     let ClosePoint = null
-    //check the closest distance for each point on the map
-    for (let obj of AllPoint){
-        if(obj.IsAvailable == false){
+        //check the closest distance for each point on the map
+    for (let obj of AllPoint) {
+        if (obj.IsAvailable == false) {
             continue
         }
-        const dx = x - parseInt(obj.point.style.left)
-        const dy = y - parseInt(obj.point.style.top)
-        const distanceSquared = dx * dx + dy * dy      
-        if (distanceSquared < BestDistance){
+        const dx = x - obj.x
+        const dy = y - obj.y
+        const distanceSquared = dx * dx + dy * dy
+        if (distanceSquared < BestDistance) {
             BestDistance = distanceSquared
-            ClosePoint= obj
+            ClosePoint = obj
         }
     }
     return ClosePoint
 }
+document.addEventListener("mousemove", function GetMouse(e) {
+    targetX = e.clientX
+    targetY = e.clientY
+});
 
-//Create X points in the screen at a random position
-for(var i = 0 ; i < 200 ;i++){
-    CreatePoint(getRandomInt(30,window.innerHeight-window.innerHeight/10),getRandomInt(30,window.innerWidth-innerWidth/15))
+//Make spider Follow cursor
+function Move() {
+    Spider1.Follow(targetX, targetY)
+    requestAnimationFrame(Move)
 }
 
-AddSpider(getRandomInt(30,window.innerHeight-window.innerHeight/10),getRandomInt(30,window.innerWidth-innerWidth/15),4)
+//Create X points in the screen at a random position
+for (var i = 0; i < 400; i++) {
+    CreatePoint(getRandomInt(30, window.innerHeight - window.innerHeight / 10), getRandomInt(30, window.innerWidth - innerWidth / 15))
+}
 
-// document.addEventListener("mousemove", Move);
-// function Move(e){
-//     var ClosestPoint = GetClosestPoint(e.clientX,e.clientY)
-//     ClosestPoint.style.background = "red"
-// }
-
+var Spider1 = new Spider(getRandomInt(30, window.innerHeight - window.innerHeight / 10), getRandomInt(30, window.innerWidth - innerWidth / 15), 10, 1)
+Move()
--- js/spider.js
+++ js/spider.js
@@ -1,27 +1,51 @@
-import { GetClosestPoint,AllPoint,context } from "./map.js"
+import { GetClosestPoint, context, canvas } from "./map.js"
 
-export function AddSpider(x,y,nblegs){
-    const spider = document.createElement("div");
-    spider.classList.add("spider")
-    spider.style.left =  x + "px"
-    spider.style.top = y+ "px"
-    document.body.appendChild(spider)
-    console.log(context)
-    for(var legs = 0 ; legs < nblegs ;legs++){
-        var NearPoint = GetClosestPoint(x,y) 
-        var PtX = parseInt(NearPoint.point.style.top.slice(0,-2))
-        var PtY = parseInt(NearPoint.point.style.left.slice(0,-2))
-        console.log(x,y,PtX,PtY)
-        NearPoint.IsAvailable = false
-        AddLegs(x,y,PtY+1,PtX+1)
+export class Spider {
+    constructor(x, y, nblegs, speed) {
+        this.x = x
+        this.y = y
+        this.nblegs = nblegs
+        this.Legs = []
+        this.body = null
+        this.speed = speed
     }
-}
-
-function AddLegs(spiderX,spiderY,PointX,pointY){
-    context.beginPath()
-    context.moveTo(spiderX, spiderY)
-    context.lineTo(PointX, pointY)
-    context.strokeStyle = "red"
-    context.lineWidth = 2
-    context.stroke()
+    Follow(x, y) {
+        var GoLEFT = this.speed
+        var GoUP = this.speed
+        if (this.x > x) {
+            GoLEFT = -this.speed
+        }
+        if (this.y > y) {
+            GoUP = -this.speed
+        }
+        this.Move(this.x + GoLEFT, this.y + GoUP)
+    }
+    Move(x, y) {
+        this.x = x
+        this.y = y
+        for (var pt of this.Legs) {
+            pt.IsAvailable = true
+        }
+        this.Legs = []
+        context.clearRect(0, 0, canvas.width, canvas.height)
+        for (var legs = 0; legs < this.nblegs; legs++) {
+            this.AddLegs()
+        }
+    }
+    AddLegs() {
+        var NearPoint = GetClosestPoint(this.x, this.y)
+        var PtX = NearPoint.x
+        var PtY = NearPoint.y
+        NearPoint.IsAvailable = false
+        this.DrawLegs(PtY + 1, PtX + 1)
+        this.Legs.push(NearPoint)
+    }
+    DrawLegs(PointX, PointY) {
+        context.beginPath()
+        context.moveTo(this.x, this.y)
+        context.lineTo(PointX, PointY)
+        context.strokeStyle = "red"
+        context.lineWidth = 2
+        context.stroke()
+    }
 }
